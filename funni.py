b07f9c43f1d96035f032b4e58878e0677d441b23
SnooComics9452 <72796659+SnooComics9452@users.noreply.github.com>
2022-09-09 21:53:25 +0800

    Add files via upload

---
+++ funni.py
@@ -0,0 +1,16 @@
+# Program make a funni weight calculator
+
+print("Body Weight Calculator")
+
+while True:
+    # take input from the user
+    num1 = float(input("Enter your weight (kg): "))
+    
+    # "calculate" the weight and output
+    print("Your weight (kg): ", num1)
+    
+    # check if user wants another calculation
+    # break the while loop if answer is no
+    choice = input("Do you want to do next calculation? (yes/no): ")
+    if choice in ('no'):
+     break
